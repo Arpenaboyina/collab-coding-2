ad8b92155f2d86dde4d3a83cba960f2a358d3b0f
AshrithaAzmeera <azmeeraashritha@gmail.com>
2025-09-05 15:41:15 +0530

    Added LCM function and unit test

---
+++ test_LCM.py
@@ -0,0 +1,11 @@
+import unittest
+from utils import lcm
+
+class TestLCM(unittest.TestCase):
+    def test_lcm(self):
+        self.assertEqual(lcm(4, 6), 12)
+        self.assertEqual(lcm(5, 7), 35)
+        self.assertEqual(lcm(10, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
